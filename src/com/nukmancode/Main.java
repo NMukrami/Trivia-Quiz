30d58472045200e25c6ae845def01b0515c85205
Nukman Mukrami <145782713+NMukrami@users.noreply.github.com>
2024-03-13 16:24:29 +0800

    commit

--- src/com/nukmancode/Main.java
+++ src/com/nukmancode/Main.java
@@ -1,6 +1,11 @@
 package com.nukmancode;
 
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Main {
@@ -12,11 +17,34 @@ public class Main {
     private static void playGame() {
         var score = 0;
         var questionNumber = 1;
-        var questionAmount = amount_question();
-        System.out.println(questionAmount);
+        var questionAmount = amountQuestion();
+        System.out.println();
+        var questions = getQuestion(questionAmount);
+        System.out.println(questions);
+
+
+
     }
 
-    private static int amount_question() {
+
+    private static String getQuestion(int questionAmount) {
+        var url = "https://opentdb.com/api.php?amount=" + questionAmount + "&category=18&type=multiple";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    private static int amountQuestion() {
         while(true) {
             try {
                 System.out.print("How many question do you want (1-50): ");
